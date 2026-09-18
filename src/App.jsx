c472753a4a31513c78f1a354fe10247810e4f158
Harshita <harshitaworld9965@gmail.com>
2026-09-18 22:08:08 +0530

    data

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,24 @@ import './App.css'
 import Header from './components/Header';
 import Intro from './components/Intro';
 import SparkCard from './components/SparkCard';
+import { sparks } from './data/sparks';
 function App(){
   return (
     <div className='app'>
       <Header />
       <main className='main-content'>
         <Intro />
-
-        <SparkCard 
-        type="COLOR"
-        title="Midnight Lavender"
-        description="Deep purple with a tiny hint of blue"
-        colors={[
-          '#8f7bb8',
-          '#57496f',
-          '#30283d',
-          '#17151c'
-        ]}
-        />
-
-        <SparkCard
-        type="IDEA"
-        title="Digital Garden"
-        description="A website where thoughts grow like plants"
-        />
-
-        <SparkCard
-        type="SONG"
-        title="Something melancholic"
-        description="Find a song that sounds like driving alone at 2 AM."
-        />
+        <section className='spark-grid'>
+        {sparks.map((spark) => (
+          <SparkCard 
+          key={spark.id}
+          type={spark.type}
+          title={spark.title}
+          description={spark.description}
+          colors={spark.colors}
+          />
+        ))}
+        </section>
       </main>
 
     </div>
---
+++ src/data/sparks.js
@@ -0,0 +1,35 @@
+export const sparks = [
+  {
+    id: 1,
+    type: 'COLOR',
+    title: 'Midnight Lavender',
+    description: 'Deep purple with a tiny hint of blue.',
+    colors: [
+      '#8f7bb8',
+      '#57496f',
+      '#30283d',
+      '#17151c'
+    ]
+  },
+
+  {
+    id: 2,
+    type: 'IDEA',
+    title: 'Digital Garden',
+    description: 'A website where thoughts grow like plants.'
+  },
+
+  {
+    id: 3,
+    type: 'SONG',
+    title: 'Something melancholic',
+    description: 'Find a song that sounds like driving alone at 2 AM.'
+  }, 
+
+  {
+    id:4,
+    type:'PLACE',
+    title:'Something Cozy',
+    description:'Something snuggly and comfortable'
+  }
+]
